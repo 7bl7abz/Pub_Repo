6b658a72e92f096b5b022ebc48accdc226dcc6df
7bl7abz <7bl7abz@gmail.com>
2024-06-22 22:55:01 +0900

    test

--- cointoss.c
+++ cointoss.c
@@ -17,6 +17,6 @@ int main(void){
         }
     }
     printf("Total Heads: %5d\n",heads);
-    printf("Total Tails: %5d\n",tails);
+    printf("Total Tails: %5d\n",tails); //test
     return 0;
 }
